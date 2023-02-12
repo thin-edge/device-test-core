87bd1d0245cda21c8bdc2544322b3f7d4ee94f42
Reuben Miller <reuben.d.miller@gmail.com>
2023-02-12 11:53:21 +0100

    fix importlib optional dependency for py3.7

--- device_test_core/__init__.py
+++ device_test_core/__init__.py
@@ -1,4 +1,8 @@
-from importlib.metadata import version, PackageNotFoundError
+import sys
+if sys.version_info < (3, 8):
+    from importlib_metadata import version, PackageNotFoundError
+else:
+    from importlib.metadata import version, PackageNotFoundError
 
 try:
     __version__ = version("device_test_core")
